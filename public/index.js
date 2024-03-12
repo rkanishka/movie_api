7e5cab90e0e7db7f85b5c695dc22d4aed0a59e18
rkanishka <143039356+rkanishka@users.noreply.github.com>
2024-03-11 22:48:00 -0700

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,126 +1,164 @@
 const express = require('express');
 const morgan = require('morgan');
-const app=express();
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Models = require("./models.js");
+const app = express();
+const Movies = Models.Movie;
+const Users = Models.User;
 
-//middleware
+mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Middleware
 app.use(morgan('common'));
 app.use(express.static('public'));
-//top movie data
-let topMovies=[
-      {
-            id:1,
-            title:'3 Idiots',
-            director:'Raj kumar Hirani' },
-      {        
-              id:2,
-              title:'PK',
-              director:'Rajkumar Hirani'},
-      {
-            id:3,
-            title:'Piku',
-            director:'Shoojit Sircar'},
-      {
-            id:4,
-            title:'Devdas',
-            director:'Sanjay Leela Bhansali' },
-      {     
-            id:5,
-            title:'Veer-Zaara',
-            director:'Yash Chopra'},
-      {      
-            id:6,
-            title:'Bhaag Milkha Bhaag',
-            director:'Rakeysh Omprakash Mehra'},
-     {
-             id:7,
-             title:'Jodha Akbar',
-             director:'Ashutosh Gowariker' } ,   
-     {
-             id:8, 
-            title:'Dangal',
-             director:'Nitesh Tiwari'},
-      {       
-               id:9,
-              title:'Queen',
-              director:'Vikas Bahl'},
-     {       
-             id:10,
-            title:'Jab We Met',
-           director:'Imtiaz Ali'}];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-      let User=[
-                  {
-                         id:1,
-                         name:'aakrati'},
-                  {
-                        id:2,
-                        name:'Bob'},
-                  {
-                          id:3,
-                         name:'Veda'} ];
+// Welcome message
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie club');
+});
 
-        app.get('/',(req,res)=>{
-             res.send('welcome to my movie club')
-          })   
-            // Routes for movies
-     app.get('/movies', (req, res) => {
-      res.send('Successful GET request returning data on all the movies');
+// Get all movies
+app.get('/movies', (req, res) => {
+  Movies.find()
+    .then((movie) => {
+      res.status(200).json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
-    //get data of specific movie
-    app.get('/movies/:movie_id', (req, res) => {
-      const movieId = req.params.movie_id;
-      res.send(`Successful GET request returning data on movie with ID ${movieId}`);
+});
+
+// Get data of a specific movie
+app.get('/movies/:movie_id', (req, res) => {
+  Movies.findOne({ _id: req.params.movie_id })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
-    // add new movie
-    app.post('/movies', (req, res) => {
-      res.send('Successful POST request creating a new movie');
+});
+
+// Add a new movie
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+
+  if (!newMovie.Title) {
+    const message = 'Missing title in request body';
+    res.status(400).send(message);
+  } else {
+    //newMovie._id = uuid.v4();
+    Movies.create(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+
+// Update movie data
+app.put('/movies/:Title/:director', (req, res) => {
+  let movie = Movies.findOne( { Title: req.params.Title });
+
+  if (movie) {
+    movie.director = req.params.director;
+    res.status(201).send('The director of the movie ' + req.params.Title + ' has been updated to ' + req.params.director);
+  } else {
+    res.status(404).send('Movie with the title ' + req.params.Title + ' was not found.');
+  }
+});
+
+
+// Delete a movie
+app.delete('/movies/:title',(req, res) => {
+  Movies.deleteOne( { Title: req.params.Title })
+    .then ((movie) =>  {
+      res.status(200).send(req.params.title + ' was deleted');
+    })
+    .catch((err) => {
+      res.status(404).send(req.params.title + ' was not found');
     });
-    // update data
-    app.put('/movies/:movie_id', (req, res) => {
-      const movieId = req.params.movie_id;
-      res.send(`Successful PUT request updating data on movie with ID ${movieId}`);
+
+});
+
+// Get all users
+app.get('/users', async (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
-    //delete movie 
-    app.delete('/movies/:movie_id', (req, res) => {
-      const movieId = req.params.movie_id;
-      res.send(`Successful DELETE request deleting movie with ID ${movieId}`);
+});
+
+// Get a user by username
+app.get('/users/:username', (req, res) => {
+  Users.findOne({ name: req.params.username})
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
     });
-    
-    // Routes for users
-    //list of all user
-    app.get('/users', (req, res) => {
-      res.send('Successful GET request returning data on all the users');
+});
+
+// Add a new user
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    Users.create(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+// Update user data
+app.put('/users/:Username/:newusername', (req, res) => {
+  let newUser = Users.findOne({name: req.params.Username});
+
+  if(newUser) {
+    newUser.name = req.params.newusername;
+   res.status(201).send('The username of the user ' + req.params.Username + ' has been updated to ' + req.params.newusername);
+  } else {
+   res.status(404).send("User with username " + req.params.Username + "was not found.");
+  }
+});
+
+// Delete a user
+app.delete('/users/:Username', (req, res) => {
+
+  Users.deleteOne( { name: req.params.Username })
+    .then ((user) =>  {
+      res.status(200).send(req.params.Username + ' was deleted');
+    })
+    .catch((err) => {
+      res.status(404).send(req.params.Username + ' was not found');
     });
-    //get data of specific user
-    app.get('/users/:user_id', (req, res) => {
-      const userId = req.params.user_id;
-      res.send(`Successful GET request returning data on user with ID ${userId}`);
-    });
-    //create new user
-    app.post('/users', (req, res) => {
-      res.send('Successful POST request creating a new user');
-    });
-    //updata data of ser
-    app.put('/users/:user_id', (req, res) => {
-      const userId = req.params.user_id;
-      res.send(`Successful PUT request updating data on user with ID ${userId}`);
-    });
-    //delet user
-    app.delete('/users/:user_id', (req, res) => {
-      const userId = req.params.user_id;
-      res.send(`Successful DELETE request deleting user with ID ${userId}`);
-    });
-    
-          app.get('/documentation',(req,res)=>{
-            res.sendFile('public/documentation.html',{root:'/Users/vtomar/movie_api'});
-           })
-          
-           //error handling middleware
-           app.use((err, req, res, next) => {
-            console.error(err.stack);
-            res.status(500).send('Something broke!');
-          });
-          //start the server
-           app.listen(8080, () => {
-            console.log('Your app is listening on port 8080.');
-          });
+});
+
+// Documentation endpoint
+app.get('/documentation', (req, res) => {
+  // Assuming you have a documentation file in the public folder
+  // Adjust the path as needed
+  res.sendFile('documentation.html', { root:'/Users/vtomar/movie_api/public' });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+// Start the server
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');});
+
---
+++ public/models.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+let movieSchema=mongoose.Schema({
+      Title:{type:String,required:true},
+      Description:{type:String,required:true},
+      Genre:{
+            name:String,
+            description:String,
+      },
+      Director:{
+            name:String,
+            bio:String
+      }
+});
+let userSchema = mongoose.Schema({
+      Name: {type: String, required: true},
+      Password: {type: String, required: true},
+      Email: {type: String, required: true},
+      Birthday: Date,
+      FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+    });
+    
+    let Movie = mongoose.model('Movie', movieSchema);
+    let User = mongoose.model('User', userSchema);
+    
+    module.exports.Movie = Movie;
+    module.exports.User = User;
