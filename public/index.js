19b0aff91d29bc2d71db78aab3361ae0f028b432
rkanishka <143039356+rkanishka@users.noreply.github.com>
2024-03-14 20:19:10 -0700

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -10,7 +10,8 @@ const Users = Models.User;
 const { check, validationResult } = require('express-validator');
 
 
-mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Middleware
 app.use(morgan('common'));
